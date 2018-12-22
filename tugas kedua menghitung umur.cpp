cb0828839a395554e137a1809abfc1143816bd7b
dickychandra21 <45659402+dickychandra21@users.noreply.github.com>
2018-12-22 20:38:44 +0700

    Add files via upload

---
+++ tugas kedua menghitung umur.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int pemasukan,pengeluaran;
+	
+	cout<<"program untuk mengetahui pemasukan dan pengeluaran"<<endl;
+	cout<<"masukan jumlah pemasukan = ";
+	cin>>pemasukan;
+	cout<<"masukan jumlah pengeluaran =";
+	cin>>pengeluaran;
+	
+	if(pemasukan>pengeluaran)
+	{
+		cout<<"pemasukan lebih kecil dari pengeluaran\n";
+		
+	}
+else
+cout<<"error";
+
+
+}
+
